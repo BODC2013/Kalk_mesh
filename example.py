ba533936f5bfd8cfa55e4e34ac44c8bd8f066075
lepsick <Lepsick2003@yandex.ru>
2024-10-18 14:50:25 +0300

    example.py

---
+++ example.py
@@ -0,0 +1,11 @@
+from Fly_Coco import Calculate
+from models import CalculateModel, SteelGrade
+
+
+calculate = Calculate(
+    cmd=CalculateModel(
+        name="1234",
+        steel_grade=SteelGrade.DOPED
+        .....
+    )
+)
